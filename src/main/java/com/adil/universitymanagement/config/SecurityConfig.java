969746cae019a0b21b8f55c5e87fb19554d11133
Adil Bashir <abashir.bscs19seecs@seecs.edu.pk>
2024-05-02 15:33:33 +0500

    Security impl commit

--- src/main/java/com/adil/universitymanagement/config/SecurityConfig.java
+++ src/main/java/com/adil/universitymanagement/config/SecurityConfig.java
@@ -5,7 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -21,10 +24,16 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/students").hasRole("TEACHER")
                         .requestMatchers(HttpMethod.PUT, "/students/update-student").hasRole("TEACHER")
                         .requestMatchers("/teacher/**").hasRole("TEACHER")
-                        .anyRequest().authenticated()
-                )
-                .httpBasic(); // Use Basic Authentication
+                        .anyRequest().authenticated());
+               // .addFilterBefore(new jwtValidator(), BasicAuthenticationFilter.class)
+              //  .csrf(csrf-> csrf.disable() )
+             //   .httpBasic(); // Use Basic Authentication
 
         return http.build();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder () {
+        return new BCryptPasswordEncoder();
     }
 }
